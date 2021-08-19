7757d941256ad783ae86dd9534e665553e87b3b0
Naplesoul <shenwhang@outlook.com>
2021-08-19 23:57:17 +0800

    Improve: use ts in vue project

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
 <template>
-  <div>
-    <login-page v-show="page==='login'" @join="page='main'"></login-page>
-    <main-page v-if="page==='main'" @back="page='login'"></main-page>
+  <div id="app">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-import loginPage from "./pages/loginPage";
-import mainPage from "./pages/mainPage";
-export default {
-  name: 'App',
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
 
+@Component({
   components: {
-    mainPage,
-    loginPage,
+    HelloWorld,
   },
-
-  data(){
-    return{
-      page : 'login',
-    }
-  },
-
-  methods :{
-  }
-};
+})
+export default class App extends Vue {}
 </script>
-<style>
-html::-webkit-scrollbar{
-  display: none;
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/service/SignalingService.ts
+++ src/service/SignalingService.ts
@@ -1,19 +1,24 @@
-import {io} from 'socket.io-client';
-import {serviceConfig, SignalType} from "../ServiceConfig";
+import {io, Socket} from 'socket.io-client';
+import {serviceConfig, SignalMethod, SignalType} from "../ServiceConfig";
 import {timeoutCallback} from "../utils/media/MediaUtils";
 
 export class SignalingService
 {
-    constructor(URL, opts, onDisconnect)
+    private readonly URL: string = null;
+    private readonly socket: Socket = null;
+    private callbackMap: Map<SignalType ,Map<SignalMethod, object>> = null;
+    private readonly disconnectCallback: () => Promise<void> = null;
+
+    constructor(URL: string, opts, onDisconnect: () => Promise<void>)
     {
         this.URL = URL;
         this.socket = io(URL, opts);
         this.disconnectCallback = onDisconnect;
         console.log('[Socket]  Start to connect');
 
-        this.callbackMap = new Map();
-        this.callbackMap.set(SignalType.request, new Map());
-        this.callbackMap.set(SignalType.notify, new Map());
+        this.callbackMap = new Map<SignalType ,Map<SignalMethod, (data) => void>>();
+        this.callbackMap.set(SignalType.request, new Map<SignalMethod, (data) => void>());
+        this.callbackMap.set(SignalType.notify, new Map<SignalMethod, (data) => void>());
 
         this.socket.on(SignalType.request, ({ method, data }) => {
             this.handleSignal(SignalType.request, method, data);
@@ -26,36 +31,36 @@ export class SignalingService
         this.socket.on('disconnect', this.disconnectCallback);
     }
 
-     handleSignal(type, method, data)
+    private handleSignal(type: SignalType, method: SignalMethod, data)
     {
         console.log(`[Socket]  Received signal (${type} , ${method})`);
-        console.log(this.callbackMap)
-        let callback = this.callbackMap.get(type).get(method);
+        let callback = this.callbackMap.get(type).get(method) as (data) => void;
         if (callback == undefined) {
-            console.log(`[Socket]  Undefined signal (${type} , ${method})`);
+            console.warn(`[Socket]  Undefined signal (${type} , ${method})`);
         } else {
             callback(data);
             console.log(`[Socket]  Signal handled (${type} , ${method})`);
         }
     }
 
-     registerListener(type, method, callback)
+    public registerListener(type: SignalType, method: SignalMethod, callback: (data) => void)
     {
         this.callbackMap.get(type).set(method, callback);
     }
 
-     removeAllListeners()
+    public removeAllListeners()
     {
         this.socket.off('disconnect', this.disconnectCallback);
-        this.callbackMap.clear();
+        this.callbackMap.get(SignalType.notify).clear();
+        this.callbackMap.get(SignalType.request).clear();
     }
 
-     waitForConnection()
+    public waitForConnection()
     {
         this.socket.connect();
-        return new Promise((resolve, reject) => {
+        return new Promise<void>((resolve, reject) => {
             console.log('[Socket]  Waiting for connection to ' + this.URL + '...');
-            let returned = false;
+            let returned: boolean = false;
             this.socket.once('connect', timeoutCallback(() => {
                 if (returned)
                     return;
@@ -82,11 +87,11 @@ export class SignalingService
         });
     }
 
-     waitForReconnection()
+    public waitForReconnection()
     {
-        return new Promise((resolve, reject) => {
+        return new Promise<void>((resolve, reject) => {
             console.log('[Socket]  Waiting for reconnection to ' + this.URL + '...');
-            let returned = false;
+            let returned: boolean = false;
             this.socket.once('connect', timeoutCallback(() => {
                 if (returned)
                     return;
@@ -109,16 +114,16 @@ export class SignalingService
         })
     }
 
-     disconnect()
+    public disconnect()
     {
         this.socket.disconnect();
     }
 
-     sendRequest(method, data = null)
+    public sendRequest(method: SignalMethod, data = null)
     {
         return new Promise((resolve, reject) => {
             if (!this.socket || !this.socket.connected) {
-                reject('No socket connection.');3
+                reject('No socket connection.');
             } else {
                 this.socket.emit(SignalType.request, { method, data },
                     timeoutCallback((err, response) => {
@@ -134,7 +139,12 @@ export class SignalingService
         });
     }
 
-     connected()
+    public sendNotify(method: SignalMethod, data = null)
+    {
+        this.socket.emit(SignalType.notify, { method, data });
+    }
+
+    public connected()
     {
         return (this.socket && this.socket.connected);
     }
