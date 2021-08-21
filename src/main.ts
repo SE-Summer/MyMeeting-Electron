383b8e2f59a4f76b16a95f0f9e27d79594b65732
KendrickChou <847361724@qq.com>
2021-08-21 10:42:59 +0800

    fix chat bug

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import global_v from './global.js'
+import {MessageType} from "@/utils/Types";
 
 Vue.config.productionTip = false
 
 Vue.prototype.GLOBAL = global_v
+Vue.prototype.MessageType = MessageType
 
 new Vue({
   vuetify,
