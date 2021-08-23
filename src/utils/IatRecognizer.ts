dfdd12bc71e5073b79d5709fb9918f9e3575c857
Naplesoul <shenwhang@outlook.com>
2021-08-23 23:34:33 +0800

    Improve: better error handling in SpeechRecognizer

--- src/utils/IatRecognizer.ts
+++ src/utils/IatRecognizer.ts
@@ -11,25 +11,24 @@ const APPID = '2d2edf67'
 const API_SECRET = 'Y2I4ZmM1Njk1NzJmN2FhNTkyYTU0ZjJh'
 const API_KEY = 'bc15fd1b2d1a690acf24b4264fbe7810'
 const url = 'wss://iat-api.xfyun.cn/v2/iat'
+const host = 'iat-api.xfyun.cn';
 const language = 'zh_cn'
 const accent = 'mandarin'
+const algorithm = 'hmac-sha256'
+const headers = 'host date request-line'
+const speechEndTimeout = 3000
 
 const workerBlob = new Blob(['(' + TranscodeWorker + ')()'], {type: "text/javascript"});
 const transWorker = new Worker(window.URL.createObjectURL(workerBlob));
 
 const getWebSocketUrl = () => {
-    return new Promise((resolve, reject) => {
-        const host = 'iat-api.xfyun.cn';
-        const apiKey = API_KEY;
-        const apiSecret = API_SECRET
+    return new Promise((resolve) => {
         // @ts-ignore
         const date = new Date().toGMTString()
-        const algorithm = 'hmac-sha256'
-        const headers = 'host date request-line'
         const signatureOrigin = `host: ${host}\ndate: ${date}\nGET /v2/iat HTTP/1.1`
-        const signatureSha = CryptoJS.HmacSHA256(signatureOrigin, apiSecret)
+        const signatureSha = CryptoJS.HmacSHA256(signatureOrigin, API_SECRET)
         const signature = CryptoJS.enc.Base64.stringify(signatureSha)
-        const authorizationOrigin = `api_key="${apiKey}", algorithm="${algorithm}", headers="${headers}", signature="${signature}"`
+        const authorizationOrigin = `api_key="${API_KEY}", algorithm="${algorithm}", headers="${headers}", signature="${signature}"`
         const authorization = btoa(authorizationOrigin)
         resolve(`${url}?authorization=${authorization}&date=${date}&host=${host}`)
     })
@@ -65,12 +64,12 @@ export class IatRecognizer
         }
     }
     // 修改录音听写状态
-    setStatus(status: RecognitionStatus) {
+    private setStatus(status: RecognitionStatus) {
         this.onStatusChange && this.status !== status && this.onStatusChange(this.status, status)
         this.status = status
     }
     // @ts-ignore
-    setResultText({ resultText, resultTextTemp, isLast } = {}) {
+    private setResultText({ resultText, resultTextTemp, isLast } = {}) {
         const text = resultTextTemp || resultText || ''
         const previousText = this.resultText || this.resultTextTemp || ''
         if (this.onRecognizerResult && text.length !== 0 && text !== previousText) {
@@ -80,7 +79,7 @@ export class IatRecognizer
         resultTextTemp !== undefined && (this.resultTextTemp = resultTextTemp)
     }
     // 连接websocket
-    connectWebSocket() {
+    private connectWebSocket() {
         return getWebSocketUrl().then((url: string) => {
             let iatWS
             if ('WebSocket' in window) {
@@ -89,12 +88,12 @@ export class IatRecognizer
                 // @ts-ignore
                 iatWS = new MozWebSocket(url)
             } else {
-                alert('浏览器不支持WebSocket')
+                this.onRecognizerError(new Error('WebSocket not supported'))
                 return
             }
             this.webSocket = iatWS
             this.setStatus(RecognitionStatus.initialized)
-            iatWS.onopen = e => {
+            iatWS.onopen = () => {
                 this.setStatus(RecognitionStatus.processing)
                 // 重新开始录音
                 setTimeout(() => {
@@ -106,14 +105,15 @@ export class IatRecognizer
             }
             iatWS.onerror = e => {
                 this.recorderStop()
+                this.onRecognizerError(e)
             }
-            iatWS.onclose = e => {
+            iatWS.onclose = () => {
                 this.recorderStop()
             }
         })
     }
     // 初始化浏览器录音
-    recorderInit() {
+    private recorderInit() {
         // @ts-ignore
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
 
@@ -123,12 +123,12 @@ export class IatRecognizer
             this.audioContext = new (window.AudioContext || window.webkitAudioContext)()
             this.audioContext.resume()
             if (!this.audioContext) {
-                alert('浏览器不支持webAudioApi相关接口')
+                this.onRecognizerError(new Error('WebAudioApi not supported'))
                 return
             }
         } catch (e) {
             if (!this.audioContext) {
-                alert('浏览器不支持webAudioApi相关接口')
+                this.onRecognizerError(new Error('WebAudioApi not supported'))
                 return
             }
         }
@@ -161,16 +161,15 @@ export class IatRecognizer
             )
         } else {
             if (navigator.userAgent.toLowerCase().match(/chrome/) && location.origin.indexOf('https://') < 0) {
-                alert('chrome下获取浏览器录音功能，因为安全性问题，需要在localhost或127.0.0.1或https下才能获取权限')
+                this.onRecognizerError(new Error('Cannot get audio due to security issues'))
             } else {
-                alert('无法获取浏览器录音功能，请升级浏览器或使用chrome')
+                this.onRecognizerError(new Error('Cannot get audio'))
             }
             this.audioContext && this.audioContext.close()
             return
         }
         // 获取浏览器录音权限成功的回调
         const getMediaSuccess = stream => {
-            console.log('getMediaSuccess')
             // 创建一个用于通过JavaScript直接处理音频
             this.scriptProcessor = this.audioContext.createScriptProcessor(0, 1, 1)
             this.scriptProcessor.onaudioprocess = e => {
@@ -188,17 +187,16 @@ export class IatRecognizer
         }
 
         const getMediaFail = (e) => {
-            alert('请求麦克风失败')
-            console.log(e)
             this.audioContext && this.audioContext.close()
             this.audioContext = undefined
             // 关闭websocket
             if (this.webSocket && this.webSocket.readyState === 1) {
                 this.webSocket.close()
             }
+            this.onRecognizerError(e);
         }
     }
-    recorderStart() {
+    private recorderStart() {
         if (!this.audioContext) {
             this.recorderInit()
         } else {
@@ -207,7 +205,7 @@ export class IatRecognizer
         }
     }
     // 暂停录音
-    recorderStop() {
+    private recorderStop() {
         // safari下suspend后再次resume录音内容将是空白，设置safari下不做suspend
         if (!(/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent))){
             this.audioContext && this.audioContext.suspend()
@@ -221,7 +219,7 @@ export class IatRecognizer
     //   this.audioData.push(...audioData)
     // }
     // 对处理后的音频数据进行base64编码，
-    toBase64(buffer) {
+    private static toBase64(buffer) {
         let binary = '';
         const bytes = new Uint8Array(buffer);
         const len = bytes.byteLength
@@ -231,7 +229,7 @@ export class IatRecognizer
         return window.btoa(binary)
     }
     // 向webSocket发送数据
-    webSocketSend() {
+    private webSocketSend() {
         if (this.webSocket.readyState !== 1) {
             return
         }
@@ -244,14 +242,14 @@ export class IatRecognizer
                 language, //小语种可在控制台--语音听写（流式）--方言/语种处添加试用
                 domain: 'iat',
                 accent, //中文方言可在控制台--语音听写（流式）--方言/语种处添加试用
-                vad_eos: 4000,
+                vad_eos: speechEndTimeout,
                 dwa: 'wpgs', //为使该功能生效，需到控制台开通动态修正功能（该功能免费）
             },
             data: {
                 status: 0,
                 format: 'audio/L16;rate=16000',
                 encoding: 'raw',
-                audio: this.toBase64(audioData),
+                audio: IatRecognizer.toBase64(audioData),
             },
         }
         this.webSocket.send(JSON.stringify(params))
@@ -287,13 +285,13 @@ export class IatRecognizer
                         status: 1,
                         format: 'audio/L16;rate=16000',
                         encoding: 'raw',
-                        audio: this.toBase64(audioData),
+                        audio: IatRecognizer.toBase64(audioData),
                     },
                 })
             )
         }, 40)
     }
-    result(resultData) {
+    private result(resultData) {
         // 识别结束
         const jsonData = JSON.parse(resultData)
         if (jsonData.data && jsonData.data.result) {
@@ -334,15 +332,15 @@ export class IatRecognizer
         }
         if (jsonData.code !== 0) {
             this.webSocket.close()
-            console.log(`${jsonData.code}:${jsonData.message}`)
+            this.onRecognizerError(new Error('Engine failure, code: ' + jsonData.code))
         }
     }
-    start() {
+    public start() {
         this.resultText = ''
         this.resultTextTemp = ''
         this.recorderStart()
     }
-    stop() {
+    public stop() {
         this.recorderStop()
     }
 }
